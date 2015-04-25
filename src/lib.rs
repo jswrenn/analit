90b8a59f5e7f7f736443de768f973ec00ec09780
John Wrenn <johnswrenn@gmail.com>
2015-04-25 11:45:16 -0400

    1D and 2D literals may now be made across any axis/plane

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,107 @@
-
 #[macro_export]
-macro_rules! analit {
-    // Starting condition
-    (+$($t:tt)+) => {analit!(0,$($t)+)};
+macro_rules! analit (
+    (                             +-- $($t:tt)+) => (analit!(X   1   ,    ,    ;$($t)+));
+    (X   $x:expr,       ,       ; --  $($t:tt)+) => (analit!(X   $x+1,    ,    ;$($t)+));
+    (X   $x:expr,       ,       ; +            ) => (($x));
+
+
+    (                             +|  $($t:tt)+) => (analit!(Y   1   ,    ,    ;$($t)+));
+    (Y   $x:expr,       ,       ; |   $($t:tt)+) => (analit!(Y   $x+1,    ,    ;$($t)+));
+    (Y   $x:expr,       ,       ; +            ) => (($x));
+
+
+    (                             +/  $($t:tt)+) => (analit!(Z   1   ,    ,    ;$($t)+));
+    (Z   $x:expr,       ,       ; /   $($t:tt)+) => (analit!(Z   $x+1,    ,    ;$($t)+));
+    (Z   $x:expr,       ,       ; +            ) => (($x));
+
     
-    /* ONE DIMENSIONAL */
-    ($dx:expr, +)               => {($dx/2)};
-    ($dx:expr, -     $($t:tt)+) => {analit!(1+$dx,$($t)+)};
+    (X   $x:expr,       ,       ; +/  $($t:tt)+) => (analit!(XZ  $x  ,1   ,    ;$($t)+));
+    (XZ  $x:expr,$y:expr,       ; /   $($t:tt)+) => (analit!(XZ  $x  ,$y+1,    ;$($t)+));
+    (XZ  $x:expr,$y:expr,       ; +   $($t:tt)+) => (($x,$y/2));
+
+
+    (X   $x:expr,       ,       ; +|  $($t:tt)+) => (analit!(XY  $x  ,1   ,    ;$($t)+));
+    (XY  $x:expr,$y:expr,       ; |   $($t:tt)+) => (analit!(XY  $x  ,$y+1,    ;$($t)+));
+    (XY  $x:expr,$y:expr,       ; +-  $($t:tt)+) => (($x,$y/2));
+
+
+    (Z   $x:expr,       ,       ; |   $($t:tt)+) => (analit!(YZ  $x  ,1   ,    ;$($t)+));
+    (YZ  $x:expr,$y:expr,       ; |   $($t:tt)+) => (analit!(YZ  $x  ,$y+1,    ;$($t)+));
+    (YZ  $x:expr,$y:expr,       ; /   $($t:tt)+) => (analit!(YZ  $x+1,$y  ,    ;$($t)+));
+    (YZ  $x:expr,$y:expr,       ; +   $($t:tt)+) => (analit!(YZ  $x  ,$y  ,    ;$($t)+));
+    (YZ  $x:expr,$y:expr,       ; +            ) => (($x/2,$y/2));
+
+
+    (XZ  $x:expr,$y:expr,       ; |   $($t:tt)+) => (analit!(XYZ $x  ,1   ,$y*2;$($t)+));
+    (XYZ $x:expr,$y:expr,$z:expr; |   $($t:tt)+) => (analit!(XYZ $x  ,$y+1,$z  ;$($t)+));
+    (XYZ $x:expr,$y:expr,$z:expr; /   $($t:tt)+) => (analit!(XYZ $x  ,$y  ,$z+1;$($t)+));
+    (XYZ $x:expr,$y:expr,$z:expr; -   $($t:tt)+) => (analit!(XYZ $x  ,$y  ,$z  ;$($t)+));
+    (XYZ $x:expr,$y:expr,$z:expr; +   $($t:tt)+) => (analit!(XYZ $x  ,$y  ,$z  ;$($t)+));
+    (XYZ $x:expr,$y:expr,$z:expr; +            ) => (($x, $y/3, $z/3));
     
-    // Depending on what character is present after the '+', we can
-    // change our parsing technique to target 2D or 3D literals.
-    ($dx:expr, + | | $($t:tt)+) => {analit!($dx,1,$($t)+)};
-    ($dx:expr, + / / $($t:tt)+) => {analit!($dx,0,2,$($t)+)};
-    
-    /* TWO DIMENSIONAL */
-    ($dx:expr, $dy:expr, +   $($t:tt)*)  => {($dx/2,$dy)};
-    // Count how many pairs of pipes there are
-    ($dx:expr, $dy:expr, | | $($t:tt)+) => {analit!($dx,$dy+1,$($t)+)};
-    
-    /* THREE DIMENSIONAL */
-    ($dx:expr, $dy:expr, $dz:expr, | $($t:tt)+) => {analit!($dx,$dy+1,$dz,$($t)+)};
-    ($dx:expr, $dy:expr, $dz:expr, / $($t:tt)+) => {analit!($dx,$dy,$dz+1,$($t)+)};
-    ($dx:expr, $dy:expr, $dz:expr, + $($t:tt)+) => {analit!($dx,$dy,$dz,$($t)+)};
-    ($dx:expr, $dy:expr, $dz:expr, - $($t:tt)+) => {analit!($dx,$dy,$dz,$($t)+)};
-    ($dx:expr, $dy:expr, $dz:expr, +)           => {($dx/2,$dy/3,$dz/3)};
-}
+);
 
 #[test]
 fn one_dimension() {
-    
-    assert_eq!(0, analit!(
-        +-+
+    assert_eq!(2, analit!(
+        +----+
     ));
     
-    assert_eq!(1, analit!(
-        +--+
+    assert_eq!(2, analit!(
+        +
+        |
+        |
+        +
+    ));
+    
+    assert_eq!(2,analit!(
+           +
+          /
+         /
+        +
     ));
 }
 
 #[test]
 fn two_dimensional() {
 
-    assert_eq!((1,1),analit!(
-        +--+
-        |  |
-        +--+
+    assert_eq!((2,1),analit!(
+        +----+
+        |    |
+        +----+
     ));
     
-    assert_eq!((8,1),analit!(
-        +----------------+
-        |                |
-        +----------------+
+    
+    assert_eq!((2,1),analit!(
+          +----+
+         /    /
+        +----+
+    ));
+    
+    assert_eq!((2,1),analit!(
+           +
+          /|
+         / +
+        + /
+        |/
+        +
+    ));
+    
+    assert_eq!((1,1),analit!(
+           +
+          /|
+         + +
+         |/
+         +
+    ));
+    
+    assert_eq!((1,2),analit!(
+           +
+          /|
+         + |
+         | +
+         |/
+         +
     ));
 }
 
